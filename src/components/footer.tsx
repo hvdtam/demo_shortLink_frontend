482ff5dbfe4b2e377d35b3bb74bdd2efe6bbeea7
Van Dinh Tam HUYNH <hvdtam@gmail.com>
2023-06-15 16:37:41 +0700

    init tailwind

---
+++ src/components/footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className="bg-white pt-4 sm:pt-10 lg:pt-12">
+      <footer className="mx-auto max-w-screen-2xl px-4 md:px-8">
+        <div className="py-8 text-center text-sm text-gray-400">© 2023 - tamk. All rights reserved.</div>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/layout.tsx
@@ -0,0 +1,18 @@
+import React, {ReactNode} from 'react';
+import Header from "@/components/header";
+import Footer from "@/components/footer";
+type Props = {
+  children: ReactNode;
+};
+
+const Layout = ({children}: Props) => {
+  return (
+    <>
+      <Header/>
+      <main>{children}</main>
+      <Footer/>
+    </>
+  );
+};
+
+export default Layout;
